0574c6108544aa6eeb4615eee1612371ba825dd9
natuyukisakura <MoriRinMonro1@gmail.com>
2024-09-14 18:39:39 +0900

    [3-14] パスワードのハッシュ化

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcryptjs"); //ハッシュ化するためのライブラリ
 
 const app = express(); // サーバー起動
 
@@ -10,12 +11,15 @@ const port = 5000;
 //新規ユーザー登録API
 app.post("/api/auth/register", (req, res) => {
   const { username, email, password } = req.body; //json形式で受け取る
+
+  const hashedPassword = bcrypt.hashSync(password, 10); //10はハッシュ化の強度
+
   const user = prisma.user.create({
     //userはテーブル名
     data: {
       username,
       email,
-      password,
+      password: hashedPassword,
     },
   });
   return res.json(user); //json形式で返す
