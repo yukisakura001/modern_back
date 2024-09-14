fadb031b2b0e61f3ba41c30b38babe5c8d44dab6
natuyukisakura <MoriRinMonro1@gmail.com>
2024-09-14 18:21:54 +0900

    3-12

--- server.js
+++ server.js
@@ -4,9 +4,4 @@ const app = express(); // サーバー起動
 
 const port = 5000;
 
-app.get(
-  "/",
-  (req, res) => res.send("<h1>Hello World!</h1>") // タグを送信
-);
-
 app.listen(port, () => console.log(`Server is running on port ${port}`)); //第二変数でサーバー起動時の処理を記述
