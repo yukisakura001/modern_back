f2fd57741e99da965543aee7193228efd06a34f5
natuyukisakura <MoriRinMonro1@gmail.com>
2024-09-14 18:49:08 +0900

    [3-15]APIテスト

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { PrismaClient } = require("@prisma/client");
-const bcrypt = require("bcryptjs"); //ハッシュ化するためのライブラリ
+const bcrypt = require("bcrypt"); //ハッシュ化するためのライブラリ
 
 const app = express(); // サーバー起動
 
@@ -8,13 +8,16 @@ const prisma = new PrismaClient(); //PrismaClientのインスタンスを作成
 
 const port = 5000;
 
+app.use(express.json()); //json形式で受け取るための記述
+
 //新規ユーザー登録API
-app.post("/api/auth/register", (req, res) => {
+app.post("/api/auth/register", async (req, res) => {
   const { username, email, password } = req.body; //json形式で受け取る
 
-  const hashedPassword = bcrypt.hashSync(password, 10); //10はハッシュ化の強度
+  const hashedPassword = await bcrypt.hashSync(password, 10); //10はハッシュ化の強度
 
-  const user = prisma.user.create({
+  const user = await prisma.user.create({
+    //レスポンスまで待つためにawaitをつける
     //userはテーブル名
     data: {
       username,
@@ -22,7 +25,7 @@ app.post("/api/auth/register", (req, res) => {
       password: hashedPassword,
     },
   });
-  return res.json(user); //json形式で返す
+  return res.json({ user }); //json形式で返す
 });
 
 app.listen(port, () => console.log(`Server is running on port ${port}`)); //第二変数でサーバー起動時の処理を記述
