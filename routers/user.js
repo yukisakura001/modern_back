7b2a3ca40cec2a4eb9f68b0dfff1e0d871305888
natuyukisakura <MoriRinMonro1@gmail.com>
2024-09-16 14:40:02 +0900

    [6-43] ログインユーザー用API作成

---
+++ routers/user.js
@@ -0,0 +1,26 @@
+const router = require("express").Router(); //ルーター分けるための記述
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt"); //ハッシュ化するためのライブラリ
+const jwt = require("jsonwebtoken"); //トークンを生成するためのライブラリ
+const isAuthenticated = require("../middlewares/isAuthenticated"); //認証用のミドルウェア
+
+const prisma = new PrismaClient(); //PrismaClientのインスタンスを作成
+
+router.post("/find", isAuthenticated, async (req, res) => {
+  try {
+    const user = await prisma.user.findUnique({
+      //findUniqueは一つのデータを取得
+      where: {
+        id: req.userId,
+      },
+    });
+    if (!user) {
+      return res.status(404).json({ message: "ユーザーが見つからない" });
+    }
+    res.status(200).json({
+      user: { id: user.id, email: user.email, username: user.username }, //userを返すとパスワードも返してしまうので、idとemailだけ返す
+    });
+  } catch (e) {
+    res.status(500).json({ message: "サーバーエラー" });
+  }
+});
